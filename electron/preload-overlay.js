aa2b56a7ef6f4d00dac849ccedba13254bc787e5
ExtremelyPowerfulCapybara <vazqueza.bernal@gmail.com>
2026-08-03 13:42:34 -0600

    feat: expose overlay settings get/subscribe on overlayAPI

--- electron/preload-overlay.js
+++ electron/preload-overlay.js
@@ -2,10 +2,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('overlayAPI', {
-  onTimerUpdate:    (cb) => ipcRenderer.on('timer-update',    (_, d) => cb(d)),
-  onEvidenceUpdate: (cb) => ipcRenderer.on('evidence-update', (_, d) => cb(d)),
-  onPlayAudio:      (cb) => ipcRenderer.on('play-audio',      (_, d) => cb(d)),
-  onResetAll:       (cb) => ipcRenderer.on('reset-all',       ()     => cb()),
-  setInteractive:   (interactive) => ipcRenderer.send('overlay-set-interactive', interactive),
-  toggleEvidence:   (index)       => ipcRenderer.send('overlay-toggle-evidence', index),
+  onTimerUpdate:     (cb) => ipcRenderer.on('timer-update',    (_, d) => cb(d)),
+  onEvidenceUpdate:  (cb) => ipcRenderer.on('evidence-update', (_, d) => cb(d)),
+  onPlayAudio:       (cb) => ipcRenderer.on('play-audio',      (_, d) => cb(d)),
+  onResetAll:        (cb) => ipcRenderer.on('reset-all',       ()     => cb()),
+  setInteractive:    (interactive) => ipcRenderer.send('overlay-set-interactive', interactive),
+  toggleEvidence:    (index)       => ipcRenderer.send('overlay-toggle-evidence', index),
+  getSettings:       ()   => ipcRenderer.invoke('overlay-settings-get'),
+  onSettingsUpdate:  (cb) => ipcRenderer.on('overlay-settings-update', (_, d) => cb(d)),
 });
